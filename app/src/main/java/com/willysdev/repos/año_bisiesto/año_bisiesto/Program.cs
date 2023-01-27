7a1f945351b236bf067f8c02a347989a6f2f3723
Willys Jimenez <willysjose2026@gmail.com>
2023-01-26 21:07:12 -0400

    solucion reto #31 .net core

---
+++ app/src/main/java/com/willysdev/repos/año_bisiesto/año_bisiesto/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Reto #31: AÑOS BISIESTOS
+ * FÁCIL | Publicación: 01 / 08 / 22 | Resolución: 08 / 08 / 22
+ * 
+ * Crea una función que imprima los 30 próximos años bisiestos
+ * siguientes a uno dado.
+ * - Utiliza el menor número de líneas para resolver el ejercicio.
+ */
+
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintNextLeapYears(300);
+    }
+
+    private static void PrintNextLeapYears(int year)
+    {
+        GregorianCalendar gregorianCalendar = new();
+        int Leap_Year_Counter = 0;
+        int NumberOfDaysInYear = gregorianCalendar.GetDaysInYear(year);
+        int newYear = year;
+
+        while(Leap_Year_Counter < 30)
+        {
+            if (NumberOfDaysInYear == 366)
+            {
+                Leap_Year_Counter++;
+                Console.WriteLine(newYear);
+            }
+            newYear++;
+            NumberOfDaysInYear = gregorianCalendar.GetDaysInYear(newYear);
+        }
+    }
+}
